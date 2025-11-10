b0592ea65b16723d963b478e9cd70ada1466c3eb
Daniel Mononen <daniel.mononen@student.hamk.fi>
2025-11-10 12:56:13 +0200

    Initial version

---
+++ src/AutoApp.java
@@ -0,0 +1,86 @@
+public class AutoApp {
+    public static void main(String[] args) throws Exception {
+
+        // luodaan auto1 olio
+        Auto auto1 = new Auto();
+        auto1.merkki = "Cadillac";
+        auto1.malli = "Escalade";
+        auto1.bensanMaara = 100;
+
+        auto1.naytaTiedot();
+        auto1.kiihdyta();
+
+        auto1.naytaTiedot();
+
+        // luodaan auto2 olio
+        Auto auto2 = new Auto("Cadillac", "STS", 0);
+
+        auto2.naytaTiedot();
+        auto2.kiihdyta();
+
+        auto2.tankkaa(50);
+        auto2.kiihdyta();
+        auto2.naytaTiedot();
+    }
+}
+
+class Auto 
+    {
+    // Attribuutit
+
+        public String merkki;
+        public String malli;
+        public int bensanMaara;
+
+    // Konstruktori
+
+    public Auto(String merkki, String malli, int bensanMaara) {
+        this.merkki = merkki;
+        this.malli = malli;
+        this.bensanMaara = bensanMaara;
+    }
+
+    public Auto() {
+        this.merkki = "Tuntematon";
+        this.malli = "Tuntematon";
+        this.bensanMaara = 0;
+    }
+
+    // Metodit
+
+    public void jarruta()
+        {
+        System.out.println("Auto jarruttaa");
+        }
+
+    public void kiihdyta()
+    {
+        if (bensanMaara <= 0) 
+        {
+            System.out.println("Ei bensaa, ei voi kiihdyttää");
+        } else {
+            System.out.println("Auto kiihtyy");
+            bensanMaara = this.bensanMaara - 1;
+        }
+
+    }
+
+    public void naytaTiedot()
+    {
+        System.out.println("Merkki: " + merkki);
+        System.out.println("Malli: " + malli);
+        System.out.println("Bensan määrä: " + bensanMaara);
+    }
+
+    public void tankkaa(int maara) 
+    {
+        System.out.println("Tankissa bensaa: " + this.bensanMaara);
+        System.out.println("Tankkaus: " + maara);
+        bensanMaara = this.bensanMaara + maara;
+        System.out.println("Tankissa bensaa tankkauksen jälkeen: " + this.bensanMaara);
+        
+    }
+
+
+
+    }
